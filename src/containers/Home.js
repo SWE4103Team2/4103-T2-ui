b3c6a4b9cc9a84361d7686d9f106f6904a41106e
rgreenan55 <rjgreenan55@gmail.com>
2021-09-23 11:57:33 -0300

    Adjusted Home margins.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,7 +8,7 @@ export const Home = () => {
   return (
     <Container maxWidth="false">
       <Grid container justifyContent="center" alignItems="center">
-        <Stack spacing={2} sx={{ marginTop: '5rem', width: '10rem' }}>
+        <Stack spacing={2} sx={{ marginTop: '7.5rem', width: '10rem' }}>
           <Button variant="contained"> Program Coordinator </Button>
           <Button variant="contained"> Accreditation Coordinators </Button>
           <Button variant="contained"> Program Advisors </Button>
