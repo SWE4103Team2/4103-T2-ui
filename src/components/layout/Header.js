cf6ef1688a85c7018f6fd35b0efead2bae4b042d
rgreenan55 <rjgreenan55@gmail.com>
2021-09-28 00:48:09 -0300

    Adjusted header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,9 +6,9 @@ import logo from '../../assets/unb.png'
 const Header = () => {
   return (
     <Grid container borderBottom={2.5} bgcolor='#ffffff70' height='3rem'>
-      <Stack alignItems='center' direction='row'> 
+      <Stack alignItems='flex-end' direction='row'> 
         <img src={logo} alt='' style={{ marginLeft: '0.5rem', maxHeight: '2.75rem' }} />
-        <h2 style={{ marginLeft: '2rem' }}> SWE4103 Team2 </h2>
+        <h1 style={{ marginLeft: '1rem' }}> SWE4103 Team2 </h1>
       </Stack>
     </Grid>
   );
