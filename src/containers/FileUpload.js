2c2064e61b4965f7ea3c439c58e93ad044d262ee
Duncan Campbell <duncanchet2001@hotmail.com>
2021-10-23 15:40:23 -0300

    cont of fileUploadPage

--- src/containers/FileUpload.js
+++ src/containers/FileUpload.js
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { Box, TextField, Input, Button, Paper, Grid, FormHelperText } from '@mui/material';
 import {useDropzone} from 'react-dropzone';
-import { borderRadius } from '@mui/system';
+import DropzoneComponent from 'react-dropzone-component';
+
 
 export const FileUpload = ({ apiFunction }) => {
 
-    const [file, setFile] = useState([]);
+    const [fileName, setFileName] = useState([]);
     const [dataSetName, setDataSetName] = useState(null);
+    const [programName, setProgramName] = useState(null);
     const {getRootProps, getInputProps, acceptedFiles} = useDropzone();
 
     useEffect(() => {
-        const files = acceptedFiles;
-        setFile(files);
+        const files = acceptedFiles.map((fileObject) => {
+            console.log(fileObject.path);
+            <li>{fileObject.path}</li>
+        });
+        console.log(files);
+        setFileName(files);
         console.log(acceptedFiles)
     }, [acceptedFiles]);
     
 
     return (
-        <Paper sx={{minWidth: '30%', minHeight: '500px'}}>
+        <Paper sx={{minWidth: '35%', maxWidth: '35%', minHeight: '500px'}}>
             <Grid container direction="column" justifyContent="center" alignItems="center">
                 <Grid item xs="12">
                     <Box display='flex'>
@@ -26,56 +32,66 @@ export const FileUpload = ({ apiFunction }) => {
                             placeholder='Dataset Name'
                             onChange={e => setDataSetName(e.target.value)}
                             sx={{ mr: '0.5rem', marginTop: 2}} />
-                        {/* <label>
-                        <Input
-                            sx={{ display: 'none' }}
-                            onClick={e => e.target.value = null}
-                            onChange={e => setFile(e.target.files[0])}
-                            accept="text/plain"
-                            type="file"
-                        />
+                        <TextField
+                            size="small"
+                            placeholder='Program'
+                            onChange={e => setProgramName(e.target.value)}
+                            sx={{ mr: '0.5rem', marginTop: 2}} />
+                
+                    </Box>
+                </Grid>
+                    <Box display='flex'>
+                        {/* <Grid item>
+                                <DropzoneComponent
+                                config={{
+                                    iconFiletypes: ['.txt'],
+                                    showFiletypeIcon: true,
+                                    postUrl: 'no-url'
+                                }}
+                                eventHandler={{addedfile: (fileItem) => console.log(fileItem)}}
+                                djsConfig={{autoProcessQueue: false}}
+                            />
+                        </Grid> */}
+                        <Grid item >
+                            <div {...getRootProps({ className: 'dropzone' })} style={{
+                                border: "black dotted 2px",
+                                borderRadius: "10px",
+                                height: "100px",
+                                
+                            }}>
+                                <input
+                                sx={{ display: '' }}
+                                // onClick={e => e.target.value = null}
+                                // onChange={e => setFile(e.target.files[0])}
+                                accept="text/plain"
+                                type="file"
+                                {...getInputProps()}
+                            />
+                            <p>Drag 'n' drop files here, or click to select files</p>
+                            {console.log(fileName)}
+                            <aside>
+                                <h4>Files</h4>
+                                <ul>
+                                    <li style={{
+                                        marginLeft: 20
+                                    }}>{fileName}</li>
+                                </ul>
+                            </aside>
+                            </div>
+                        </Grid>
+                    </Box>
+                    <Grid>
                         <Button
                             variant="contained"
                             size="small"
                             component="span"
-                            disabled={!dataSetName}
-                            sx={{ height: '70%', marginTop: 2 }}
+                            disabled={!dataSetName && !programName}
+                            sx={{ height: '70%'}}
                         > 
-                            Upload File
+                           Submit
                         </Button>
-                        </label> */}
-                        
-                        <div {...getRootProps({ className: 'dropzone' })} style={{
-                            border: "black solid 1px",
-                            borderRadius: "10px",
-                            height: "100px",
-                        }}>
-                            <input
-                            sx={{ display: '' }}
-                            // onClick={e => e.target.value = null}
-                            // onChange={e => setFile(e.target.files[0])}
-                            accept="text/plain"
-                            type="file"
-                            {...getInputProps()}
-                        />
-                         <p>Drag 'n' drop some files here, or click to select files</p>
-                         {console.log(file)}
-                         <aside>
-                            <h4>Files</h4>
-                            <ul>
-                                <li>{file}</li>
-                            </ul>
-                        </aside>
-                        </div>
-                    </Box>
-                </Grid>
-                {/* <Grid item>
-                    {console.log(file)}
-                    <FormHelperText>{file.name}</FormHelperText>
-                </Grid> */}
-                <Grid>
-                    
-                </Grid>
+                    </Grid>
+            
             </Grid>
         </Paper>
     );
