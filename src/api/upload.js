db8ccd61749446ba19a9cfa5e073df1b8ad50885
rgreenan55 <rjgreenan55@gmail.com>
2021-10-25 13:50:30 -0300

    Added program

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,7 +1,7 @@
 import api from './api';
 
-export const uploadFilesAPI = async (fileName, files) => {
-  const result = await api.post('/upload', { params: { fileName, files }});
+export const uploadFilesAPI = async (fileName, program, files) => {
+  const result = await api.post('/upload', { params: { fileName, program, files }});
 
   return result.data;
 };
